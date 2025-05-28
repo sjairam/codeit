37d0104971cedf98b655b505b652b55bce497455
sjairam <sanjay.jairam.eu@gmail.com>
2025-05-28 09:27:43 +0100

    Update biggest_files

---
+++ bin_shell/biggest_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FOLDER="$1"
+
+# Check if a parameter was given
+if [ -z "$FOLDER" ]; then
+  echo "Usage: $0 <folder>"
+  exit 1
+fi
+
+echo "🔎 Searching for the biggest files in: $1"
+echo "--------------------------------------------"
+
+find "$1" -type f -exec du -h {} + 2>/dev/null | sort -rh | head -n 10
+
