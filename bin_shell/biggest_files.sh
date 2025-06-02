2a355f25c6703093153c0370c35b27aaea3d5353
sjairam <sanjay.jairam.eu@gmail.com>
2025-06-02 15:46:37 +0100

    Update biggest_files

--- bin_shell/biggest_files.sh
+++ bin_shell/biggest_files.sh
@@ -11,5 +11,5 @@ fi
 echo "🔎 Searching for the biggest files in: $1"
 echo "--------------------------------------------"
 
-find "$1" -type f -exec du -h {} + 2>/dev/null | sort -rh | head -n 10
+find "$1" -type f -exec du -h {} + 2>/dev/null | sort -rh | head -n 20
 
